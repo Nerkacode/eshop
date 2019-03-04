15111f104fb3ac79dc00669a75a3999b385c9df1
Nerkacode <nerijus.bareikis@gmail.com>
2019-03-04 21:30:25 +0200

    added product cards"

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Shop } from './pages'; //pagal defaulta ieskomas pages.js, jei nera tada pages katalogo ir jame index.js failo
+import { PulseLoader } from 'react-spinners';
+import { Shop } from './pages'; // pagal defaulta ieskomas pages.js, jei nera tada pages katalogo ir jame index.js failo
 import { PageLayout } from './components';
 
 const NAV_LINKS = ['shop', 'cart', 'favorites'].map(link => (
@@ -14,25 +15,28 @@ class App extends React.Component {
     this.state = {
       products: [],
       error: null,
+      loading: false,
     };
   }
 
   componentDidMount() {
+    this.setState({ loading: true });
     fetch('https://boiling-reaches-93648.herokuapp.com/food-shop/products')
       .then(response => response.json())
-      .then(json => this.setState({ products: json }))
-      .catch(() => this.setState({ error: 'Something Went Wrong' }));
+      .then(json => this.setState({ products: json, loading: false }))
+      .catch(() =>
+        this.setState({ error: 'Something Went Wrong', loading: false })
+      );
   }
 
   render() {
-    const { products } = this.state;
+    const { products, loading, error } = this.state;
 
     return (
       <PageLayout navLinks={NAV_LINKS}>
-        {products.map((product, i) => (
-          <p key={i}>{product.name}</p>
-        ))}
-        <Shop />
+        {loading && <PulseLoader />}
+        {error && <h3>{error}</h3>}
+        <Shop products={products} />
       </PageLayout>
     );
   }
--- src/app/pages/Shop.js
+++ src/app/pages/Shop.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './index.scss';
 
-function Shop() {
-  return <div>Here will be my shop.</div>;
+function Shop({ products }) {
+  return (
+    <div className="products">
+      {products.map(product => (
+        <div className="productCard" key={product.id}>
+          <img src={product.image} alt={product.name} />
+          <p>{product.name}</p>
+          <p>
+            {product.price}
+            <span> {product.currency}</span>
+          </p>
+          <button type="button" onClick={() => console.log('add to chart')}>
+            <span role="img" aria-label="close">
+              👜
+            </span>
+          </button>
+          <button type="button" onClick={() => console.log('like product')}>
+            <span role="img" aria-label="close">
+              👍
+            </span>
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 }
+
+Shop.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+Shop.defaultProps = {
+  products: [],
+};
 
 export default Shop;
